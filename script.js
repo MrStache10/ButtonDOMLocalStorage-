66bc82342070f77a0694cbf9c7745501803c568d
MrStache10 <victord92@nycstudents.net>
2025-01-14 11:57:55 -0500

    Update script.js
    Updated script

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ JAVASCRIPT
         const header = document.getElementById('header');
         const button = document.getElementById('click-button');
        
-//1.Add a comment here explaining the let clickCount variable  
+//1.Add a comment here explaining the let clickCount variable 
+//Evertime you click the button, the UI changes each time adding a new color
         let clickCount = localStorage.getItem('clickCount') ? parseInt(localStorage.getItem('clickCount')) : 0;
 
         // Update display on load
@@ -11,6 +12,7 @@ JAVASCRIPT
         updateUI();
 
 //2.Explain what this event listener function does (and what function it calls).
+// Waits for an event to occur then it responds.
         // Add event listener to button
         button.addEventListener('click', () => {
             clickCount++;
@@ -20,6 +22,7 @@ JAVASCRIPT
         });
 
 //3.Explain what function updateUI does and is holding the colors and texts
+// A change to the products user interface, to make it easier to move and change things.
         function updateUI() {
             const colors = ['#FF5733', '#33FF57', '#3357FF', '#F5A623', '#E91E63'];
             const texts = [

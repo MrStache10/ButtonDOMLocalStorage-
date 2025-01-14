4fccbc3e95fee155333b9e3bbd397d1d22c44331
MrStache10 <victord92@nycstudents.net>
2025-01-14 12:05:56 -0500

    Update script.js
    kys

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-JAVASCRIPT
+
  const clickCountDisplay = document.getElementById('click-count');
         const header = document.getElementById('header');
         const button = document.getElementById('click-button');
